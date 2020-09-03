506fb58045ca1e1501e2714f78939744f82c2e25
Jon Jagger <jon@jaggersoft.com>
2020-09-03 16:17:22 +0100

    Fix containers-down script

--- sh/containers_down.sh
+++ sh/containers_down.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -Eeu
 
+source "${SH_DIR}/augmented_docker_compose.sh"
+
 # - - - - - - - - - - - - - - - - - - - - - -
 containers_down()
 {
-  docker-compose \
-    --file "${ROOT_DIR}/docker-compose.yml" \
+  augmented_docker_compose \
     down \
     --remove-orphans \
     --volumes
