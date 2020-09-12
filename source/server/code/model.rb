8b4a567dcf62b8bf50bbb1e80c619361a935dc57
Jon Jagger <jon@jaggersoft.com>
2020-09-12 17:14:24 +0100

    Refactoring; renaming

--- source/server/code/model.rb
+++ source/server/code/model.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-require_relative 'model/id_generator'
 require_relative 'model/id_pather'
 require_relative 'model/group_v0'
 require_relative 'model/group_v1'
 require_relative 'model/kata_v0'
 require_relative 'model/kata_v1'
-require_relative 'model/oj_adapter'
+require 'json'
 
 class Model
 
@@ -20,12 +19,13 @@ class Model
   end
 
   def group_manifest(id:)
-    group(version_from_group_id(id)).manifest(id)
+    group(version_group(id)).manifest(id)
   end
 
   def group_create(manifests:, options:)
     manifest = manifests[0]
-    group(version_from_manifest(manifest)).create(manifest, options)
+    version = (manifest['version'] || CURRENT_VERSION).to_i
+    group(version).create(manifest, options)
   end
 
   #- - - - - - - - - - - - - - - - - -
@@ -35,50 +35,49 @@ class Model
   end
 
   def kata_manifest(id:)
-    kata(version_from_kata_id(id)).manifest(id)
+    kata(version_kata(id)).manifest(id)
   end
 
   def kata_create(manifest:, options:)
-    kata(version_from_manifest(manifest)).create(manifest, options)
+    version = (manifest['version'] || CURRENT_VERSION).to_i
+    kata(version).create(manifest, options)
   end
 
   private
 
-  include IdPather # group_id_path, kata_id_path
-  include OjAdapter
+  attr_reader :externals
+
+  include IdPather
 
   def group(version)
-    [ Group_v0, Group_v1 ][version].new(@externals)
+    GROUPS[version].new(externals)
   end
 
   def kata(version)
-    [ Kata_v0, Kata_v1 ][version].new(@externals)
+    KATAS[version].new(externals)
   end
 
-  def version_from_group_id(id)
-    version_from_path(group_id_path(id, 'manifest.json'))
+  def version_group(id)
+    version_path(group_id_path(id, 'manifest.json'))
   end
 
-  def version_from_kata_id(id)
-    version_from_path(kata_id_path(id, 'manifest.json'))
+  def version_kata(id)
+    version_path(kata_id_path(id, 'manifest.json'))
   end
 
-  def version_from_path(path)
+  def version_path(path)
     manifest_src = saver.assert(saver.file_read_command(path))
-    manifest = json_parse(manifest_src)
+    manifest = JSON.parse!(manifest_src)
     # if manifest['version'].nil?
     # then nil.to_i ==> 0 which is what we want
     manifest['version'].to_i
   end
 
-  CURRENT_VERSION = 1
-
-  def version_from_manifest(manifest)
-    (manifest['version'] || CURRENT_VERSION).to_i
-  end
-
   def saver
-    @externals.saver
+    externals.saver
   end
 
+  CURRENT_VERSION = 1
+  GROUPS = [ Group_v0, Group_v1 ]
+  KATAS = [ Kata_v0, Kata_v1 ]
 end
