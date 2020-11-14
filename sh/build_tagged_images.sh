d0e5d1cdfc35dc14348e913890b219f8bcb5dc05
Jon Jagger <jon@jaggersoft.com>
2020-11-14 13:18:59 +0000

    Tidy build script.

--- sh/build_tagged_images.sh
+++ sh/build_tagged_images.sh
@@ -5,38 +5,25 @@ source ${SH_DIR}/augmented_docker_compose.sh
 # - - - - - - - - - - - - - - - - - - - - - -
 build_tagged_images()
 {
-  local -r dil=$(docker image ls --format "{{.Repository}}:{{.Tag}}")
-  remove_all_but_latest "${dil}" "${CYBER_DOJO_MODEL_IMAGE}"
-  remove_all_but_latest "${dil}" "${CYBER_DOJO_MODEL_CLIENT_IMAGE}"
-
   echo
   augmented_docker_compose \
     build \
     --build-arg COMMIT_SHA=$(git_commit_sha)
-
-  docker tag ${CYBER_DOJO_MODEL_IMAGE}:$(image_tag) ${CYBER_DOJO_MODEL_IMAGE}:latest
-  docker tag ${CYBER_DOJO_MODEL_CLIENT_IMAGE}:$(image_tag) ${CYBER_DOJO_MODEL_CLIENT_IMAGE}:latest
-
-  check_embedded_env_var
-  echo
-  echo "echo CYBER_DOJO_MODEL_SHA=${CYBER_DOJO_MODEL_SHA}"
-  echo "echo CYBER_DOJO_MODEL_TAG=${CYBER_DOJO_MODEL_TAG}"
-  echo
 }
 
 # - - - - - - - - - - - - - - - - - - - - - -
-remove_all_but_latest()
+tag_images_to_latest()
 {
-  local -r docker_image_ls="${1}"
-  local -r name="${2}"
-  for image_name in `echo "${docker_image_ls}" | grep "${name}:"`
-  do
-    if [ "${image_name}" != "${name}:latest" ]; then
-      if [ "${image_name}" != "${name}:<none>" ]; then
-        docker image rm "${image_name}"
-      fi
-    fi
-  done
+  docker tag ${CYBER_DOJO_MODEL_IMAGE}:$(image_tag) ${CYBER_DOJO_MODEL_IMAGE}:latest
+  docker tag ${CYBER_DOJO_MODEL_CLIENT_IMAGE}:$(image_tag) ${CYBER_DOJO_MODEL_CLIENT_IMAGE}:latest
+}
+
+# - - - - - - - - - - - - - - - - - - - - - -
+show_env_vars()
+{
+  echo "echo CYBER_DOJO_MODEL_SHA=${CYBER_DOJO_MODEL_SHA}"
+  echo "echo CYBER_DOJO_MODEL_TAG=${CYBER_DOJO_MODEL_TAG}"
+  echo
 }
 
 # - - - - - - - - - - - - - - - - - - - - - -
